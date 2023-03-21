5658db0b70394192c3ee6f2e867a0b690b39ce9d
HanielCota <hanielcota@hotmail.com>
2023-03-21 12:01:52 -0300

    added .gitignore

--- src/main/java/com/ankares/combatlog/CombatLog.java
+++ src/main/java/com/ankares/combatlog/CombatLog.java
@@ -1,7 +1,11 @@
 package com.ankares.combatlog;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
 public class CombatLog extends JavaPlugin {
 
-
-
+    @Override
+    public void onEnable() {
+        // TODO document why this method is empty
+    }
 }
