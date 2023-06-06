4c106db565dca439756d36478b66641234f5452f
HanielCota <hanielcota@hotmail.com>
2023-06-06 14:38:28 -0300

    Small Updates.

--- src/main/java/com/github/hanielcota/combatlog/CombatLogPlugin.java
+++ src/main/java/com/github/hanielcota/combatlog/CombatLogPlugin.java
@@ -6,6 +6,7 @@ public final class CombatLogPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        System.out.println("CombatLogPlugin has been enabled.");
+        // Plugin startup logic
     }
+
 }
---
+++ src/main/java/com/github/hanielcota/combatlog/utils/Author.java
@@ -0,0 +1,19 @@
+package com.github.hanielcota.combatlog.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Author {
+
+    private final String name;
+    private final String email;
+    private final String github;
+    private final String discord;
+
+
+}
+
+
+
