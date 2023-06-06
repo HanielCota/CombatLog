6f904e2abe271f1956825815f1da12d552312ff9
HanielCota <hanielcota@hotmail.com>
2023-06-06 12:54:07 -0300

    Small Updates from dependencies.

---
+++ src/main/java/com/github/hanielcota/combatlog/CombatLogPlugin.java
@@ -0,0 +1,11 @@
+package com.github.hanielcota.combatlog;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CombatLogPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("CombatLogPlugin has been enabled.");
+    }
+}
